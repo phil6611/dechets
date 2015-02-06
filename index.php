718a4b0aa990205be9d78f9b033a69cc64bd7105
phil6611 <philippe@philippe-poisse.eu>
2015-02-06 23:20:49 +0100

    Code html de la page d'accueil

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
+<!DOCTYPE html>
 <html>
-
-	<head>
-		<title>Carte des déchets</title>
-	</head>
-
-	<body>
-		<h1>Carte des déchets</h1>
-	</body>
-
+    <head>
+        <title>Carte des déchets</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Carte des déchets</div>
+        
+        <div id="map" />
+    </body>
 </html>
